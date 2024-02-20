709260535eb476343494858be514f2be01b3ed01
wellmota <impeto.wellington@gmail.com>
2024-02-20 14:11:09 -0300

    Creating blog page body and filling content from api

---
+++ js/app.js
@@ -0,0 +1,82 @@
+const url = "http://jsonplaceholder.typicode.com/posts";
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+const postContainer = document.querySelector("#post-container");
+
+const postPage = document.querySelector("#post");
+const commentsContainer = document.querySelector("#comments-container");
+
+// Get id from URL
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const postId = urlSearchParams.get("id");
+
+// get all posts
+async function getAllPosts() {
+  const response = await fetch(url);
+  const data = await response.json();
+  loadingElement.classList.add("hide");
+  data.map((post) => {
+    const div = document.createElement("div");
+    const title = document.createElement("h2");
+    const body = document.createElement("p");
+    const link = document.createElement("a");
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "Ler";
+    link.setAttribute("href", `/post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+
+    postsContainer.appendChild(div);
+  });
+}
+
+// Get individual posts
+
+async function getPost(id) {
+  const [responsePost, responseComments] = await Promise.all([
+    fetch(`${url}/${id}`),
+    fetch(`${url}/${id}/comments`),
+  ]);
+  const dataPost = await responsePost.json();
+  const dataComments = await responseComments.json();
+  loadingElement.classList.add("hide");
+  postPage.classList.remove("hide");
+
+  const title = document.createElement("h1");
+  const body = document.createElement("p");
+
+  title.innerText = dataPost.title;
+  body.innerText = dataPost.body;
+
+  postContainer.appendChild(title);
+  postContainer.appendChild(body);
+
+  dataComments.map((comment) => {
+    createComment(comment);
+  });
+}
+
+function createComment(comment) {
+  const div = document.createElement("div");
+  const email = document.createElement("h3");
+  const commentBody = document.createElement("p");
+
+  email.innerText = comment.email;
+  commentBody.innerText = comment.body;
+
+  div.appendChild(email);
+  div.appendChild(commentBody);
+
+  commentsContainer.appendChild(div);
+}
+
+if (!postId) {
+  getAllPosts();
+} else {
+  getPost(postId);
+}
